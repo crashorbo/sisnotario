64f3994cef81366e46d6fffcba7647d7d691bfe7
Alexander Calani Apaza <alexander@MacBook-Air-de-Alexander.local>
2019-04-26 15:15:44 -0400

    conflictos dist

--- notario/settings.py
+++ notario/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8e0m19#9ffp7o=ylnwrfi^2kz8com9shq79=z#-9=pi0_u*i8z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*',]
 
